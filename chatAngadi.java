fa95b3b35d7866989ef3d08bbf71fa80e69ae86a
Arpitha234 <arpithamanjunath02@gmail.com>
2023-07-13 14:54:38 +0530

    Commiting the array and for loop

---
+++ chatAngadi.java
@@ -0,0 +1,16 @@
+class chatAngadi{
+public static void main(String angadi[]){
+
+
+String chatNames[] ={"PaniaPuri","PavBaaji","Samosa","Churmuri","SevPuri"};
+getchatNames(chatNames);
+}
+public static void getchatNames(String chatNames[]){
+for(String chatName : chatNames){
+System.out.println(chatName);
+}
+
+}
+}
+
+
